3727c1be8075ae1f6c9a207ecb8ef49f188caa32
Prem Sagar Mishra <iamknocki@gmail.com>
2024-08-21 10:05:39 +0530

    Completed Responsiveness

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import Home from "../page/Home"
-import About from "../page/About"
-import Skills from "../page/Skills"
-import Services from "../page/Services"
-import Contact from "../page/Contact"
-import Projects from "../page/Projects"
-import { BrowserRouter as Router,Routes,Route ,Navigate} from "react-router-dom"
+import React from 'react';
+import Home from "../page/Home";
+import About from "../page/About";
+import Skills from "../page/Skills";
+import Services from "../page/Services";
+import Contact from "../page/Contact";
+import Projects from "../page/Projects";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const MainBody = () => {
   return (
-
     <Router>
-    <div className='body'>
-        <div style={{position:"relative",top:"0%",left:"0%",zIndex:"-1"}}>
-        <div style={{height:"21px",background:"var(--main-color)",width:"260px"}}></div>
-        <div style={{height:"260px",background:"var(--main-color)",width:"21px"}}></div>
+      <div className='body'>
+        {/* Top-left background elements */}
+        <div style={{ position: "absolute", top: 0, left: 0, zIndex: -1 }}>
+          <div style={{ height: "21px", background: "var(--main-color)", width: "260px" }}></div>
+          <div style={{ height: "260px", background: "var(--main-color)", width: "21px" }}></div>
         </div>
-        {/* Here complete Components will come */}
+
+        {/* Main Content */}
         <div className='body2'>
-           <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/about" element={<About />}/>
-           <Route path="/skills" element={<Skills />}/>
-           <Route path="/services" element={<Services />}/>
-           <Route path="/contact" element={<Contact />}/>
-           <Route path="/projects" element={<Projects />}/>
-           </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
         </div>
 
-        <div style={{position:"relative",rotate:"180deg",zIndex:"-1"}}>
-        <div style={{height:"21px",background:"var(--main-color)",width:"260px"}}></div>
-        <div style={{height:"260px",background:"var(--main-color)",width:"21px"}}></div>
+        {/* Bottom-right background elements */}
+        <div style={{ position: "absolute",rotate:"180deg", bottom: 0, right: 0, zIndex: -1 }}>
+          <div style={{ height: "21px", background: "var(--main-color)", width: "260px" }}></div>
+          <div style={{ height: "260px", background: "var(--main-color)", width: "21px", transform: "rotate(180deg)" }}></div>
         </div>
-        
-    </div>
+      </div>
     </Router>
-  )
-}
+  );
+};
 
-export default MainBody
+export default MainBody;
