8beb89f322a7b7a8974e00545fd55dfc09e58283
Prem Sagar Mishra <iamknocki@gmail.com>
2024-08-24 00:00:49 +0530

    Fixed bugs and made hamburger for mobile viewport

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Style.css"
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className='navbar'>
-        <div className="left">
+      <div className="left">
         <a href="./">Portfolio</a>
-        </div>
-        <div className="right">
-            
-                <li><a href="./about">About</a></li>
-                <li><a href="./services">Services</a></li>
-                <li><a href="./projects">Projects</a></li>
-                <li><a href="./skills">Skills</a></li>
-                <li><a href="./contact">Contact</a></li>
-            
-        </div>
+      </div>
+      <div className={`right ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="./about" onClick={toggleMenu}>About</a></li>
+          <li><a href="./services" onClick={toggleMenu}>Services</a></li>
+          <li><a href="./projects" onClick={toggleMenu}>Projects</a></li>
+          <li><a href="./skills" onClick={toggleMenu}>Skills</a></li>
+          <li><a href="./contact" onClick={toggleMenu}>Contact</a></li>
+        </ul>
+      </div>
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
     </div>
   )
 }
