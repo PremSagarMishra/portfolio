838cf4c202da631b2d2bbc895044c291f0a5d168
Prem Sagar Mishra <iamknocki@gmail.com>
2024-09-02 20:48:59 +0530

    Changed image for women safety app

--- src/page/Project-list.js
+++ src/page/Project-list.js
@@ -1,4 +1,5 @@
 import Project1 from "../assets/p1.png"
+import Project2 from "../assets/p2.png"
 import Project3 from "../assets/p3.png"
 import Project4 from "../assets/p4.png"
 
@@ -10,14 +11,13 @@ export const projects = [
       "I developed a library management system. This system enabled administrators to track book transactions, manage book inventory, and oversee transaction details such as status, fines, and return dates. I provided students with accurate oversight of book inventory and allowed for the updating or deletion of student profiles. I created the complete front-end and back-end system from scratch, ensuring comprehensive functionality for all features.",
     techstack: ["React.js", "Node.js", "Express.js", "MongoDB", "Git"],
     github:"https://github.com/PremSagarMishra/LibraryManagementSystem"
-  },
-  {
-    img: Project3,
-    heading: "Women Safety App",
+  },{
+    img: Project4,
+    heading: "Personal Portfolio",
     content:
-      "I developed an Android app that enabled pre-registration of emergency contacts and provided a one-button option for instant location sharing or audio recording during emergencies. I integrated Firebase OTP for secure user authentication and created customizable settings for users to personalize their preferences, enhancing functionality and overall user experience.",
-    techstack: ["Java","Android Studion", "XML", "Firebase","Git"],
-    github:"https://github.com/PremSagarMishra/WomenSafetyApp"
+      "I built a personal portfolio website that showcases my skills and the projects I've worked on. The portfolio includes a contact feature, allowing visitors to reach out directly. Before diving into coding, I designed the UI and UX using Figma to ensure a user-friendly experience. The website is fully responsive, making it suitable for all devices, and I added some cool animations to enhance its visual appeal. I developed the portfolio using ReactJS and utilized Git for version control throughout the project.",
+    techstack: ["React.js", "Git"],
+    github:"https://github.com/PremSagarMishra/portfolio"
   },
   {
     img: Project3,
@@ -27,12 +27,12 @@ export const projects = [
     techstack: ["React.js", "Next.js", "Git"],
     github:"https://github.com/PremSagarMishra/5cdn-assignment"
   },{
-    img: Project4,
-    heading: "Personal Portfolio",
+    img: Project2,
+    heading: "Women Safety App",
     content:
-      "I built a personal portfolio website that showcases my skills and the projects I've worked on. The portfolio includes a contact feature, allowing visitors to reach out directly. Before diving into coding, I designed the UI and UX using Figma to ensure a user-friendly experience. The website is fully responsive, making it suitable for all devices, and I added some cool animations to enhance its visual appeal. I developed the portfolio using ReactJS and utilized Git for version control throughout the project.",
-    techstack: ["React.js", "Git"],
-    github:"https://github.com/PremSagarMishra/portfolio"
+      "I developed an Android app that enabled pre-registration of emergency contacts and provided a one-button option for instant location sharing or audio recording during emergencies. I integrated Firebase OTP for secure user authentication and created customizable settings for users to personalize their preferences, enhancing functionality and overall user experience.",
+    techstack: ["Java","Android Studion", "XML", "Firebase","Git"],
+    github:"https://github.com/PremSagarMishra/WomenSafetyApp"
   },
   
   
